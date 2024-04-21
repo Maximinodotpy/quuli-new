353eaa5e5439723d3c73d00816d1ef9ceb95547a
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2024-04-21 06:25:18 +0200

    R: checkQuestionFormData function since a route needs it

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -29,3 +29,38 @@ export function hasDuplicates(array: Array<any>, remove_empty_strings = true) {
 
   return (new Set(array)).size !== array.length;
 }
+
+export async function checkQuestionFormData(formData: FormData) {
+  const questionO: Question = {
+    question: formData.get("frage") as string,
+    answer: formData.get("antwort") as string,
+    wrongAnswer1: String(formData.get("falsch1") || '') as string,
+    wrongAnswer2: String(formData.get("falsch2") || '') as string,
+    wrongAnswer3: String(formData.get("falsch3") || '') as string,
+    categoryId: String(formData.get("category") as string),
+  } as Question;
+
+  // Trim all the strings
+  questionO.question = questionO.question.trim();
+  questionO.answer = questionO.answer.trim();
+  questionO.wrongAnswer1 = questionO.wrongAnswer1.trim();
+  questionO.wrongAnswer2 = questionO?.wrongAnswer2.trim();
+  questionO.wrongAnswer3 = questionO?.wrongAnswer3.trim();
+
+  // Check if there is a question
+  if (!questionO.question) {
+    throw new Error("Keine Frage eingegeben");
+  }
+
+  // Check if there is an answer
+  if (!questionO.answer) {
+    throw new Error("Keine Antwort eingegeben");
+  }
+
+  // Check if there is at least one wrong answer
+  if (!questionO.wrongAnswer1 && !questionO.wrongAnswer2 && !questionO.wrongAnswer3) {
+    throw new Error("Keine falsche Antwort eingegeben");
+  }
+
+  return questionO;
+}
