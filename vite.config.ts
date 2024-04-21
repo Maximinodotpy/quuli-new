1ed857ae74aead35ce37fc68d6c7a6e397da3499
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2024-04-21 06:59:11 +0200

    Something

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+// vite.config.ts
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { createRequire } from 'module'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-export default defineConfig({
+const { resolve } = createRequire(import.meta.url)
+
+const prismaClient = `prisma${path.sep}client`
+
+const prismaClientIndexBrowser = resolve('@prisma/client/index-browser').replace(`@${prismaClient}`, `.${prismaClient}`)
+
+export default defineConfig(() => ({
 	plugins: [sveltekit()],
-});
+   resolve: { alias: { '.prisma/client/index-browser': path.relative(__dirname, prismaClientIndexBrowser) } },
+}))
